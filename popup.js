548669ebec9bb8a7fd3937dc643bd62f913a587e
chancecorbett <165570327+chancecorbett@users.noreply.github.com>
2024-03-31 10:18:38 -0400

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,11 @@
+document.getElementById('changeColor').addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+function setPageBackgroundColor() {
+  document.body.style.backgroundColor = 'pink';
+}
